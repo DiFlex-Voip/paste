c63478c51ec807a956836e1ed285280294c8be6b
GGKapanadze <doublegkapanadze@gmail.com>
2022-10-31 16:31:03 +0400

    refactor: added typings with bumped node version

---
+++ types/entry.d.ts
@@ -0,0 +1,10 @@
+function parseCliboard(
+  e: Event,
+  options: {
+    rowSelector: string;
+    cellSelector: string;
+    inputSelector: string;
+  }
+): void;
+
+function parseData(data: string): string;
